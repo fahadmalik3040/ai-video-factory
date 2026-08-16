367681cfb5c214f362a74e8fc93d9529e77ec411
Fahad Malik <fahadmalik3040@gmail.com>
2026-08-16 10:49:01 +0530

    Critical Fix: Removed Drei PerspectiveCamera to permanently eliminate layout effect infinite loops in headless rendering

--- src/components/SceneRouter.tsx
+++ src/components/SceneRouter.tsx
@@ -1,7 +1,6 @@
 import { ThreeCanvas } from '@remotion/three';
 import { AbsoluteFill } from 'remotion';
 import { MasterScene } from '../scenes/MasterScene';
-import { PerspectiveCamera } from '@react-three/drei';
 import { AudioEngine } from '../engine/audio/AudioEngine';
 
 export const SceneRouter = ({ sceneData }: any) => {
@@ -12,18 +11,16 @@ export const SceneRouter = ({ sceneData }: any) => {
     <AbsoluteFill style={{ backgroundColor: '#020202' }}>
       <AudioEngine category={sceneData?.theme || "default"} />
       
-      {/* 3D Background - Strictly WebGL */}
       <ThreeCanvas 
         width={3840} 
         height={2160} 
+        camera={{ position: [0, 0, 20], fov: 50 }} 
         style={{ position: 'absolute', top: 0, left: 0 }}
         gl={{ preserveDrawingBuffer: true, antialias: false, alpha: false }}
       >
-         <PerspectiveCamera makeDefault position={[0, 0, 20]} fov={50} />
          <MasterScene data={sceneData} />
       </ThreeCanvas>
 
-      {/* 2D Crisp Text Overlay - Immune to WebGL Layout Crashes */}
       <AbsoluteFill style={{
         justifyContent: 'center',
         alignItems: 'center',
