ac4e3014f424a5366280773dde4f224f19e923bb
Fahad Malik <fahadmalik3040@gmail.com>
2026-08-16 10:55:59 +0530

    Critical Fix: Hardcoded explicit pixel dimensions in ThreeCanvas style to bypass WebGL ResizeObserver infinite layout loops

--- src/components/SceneRouter.tsx
+++ src/components/SceneRouter.tsx
@@ -8,20 +8,31 @@ export const SceneRouter = ({ sceneData }: any) => {
   const glowColor = sceneData?.lighting?.colorTheme || "#00ffff";
 
   return (
-    <AbsoluteFill style={{ backgroundColor: '#020202' }}>
+    <AbsoluteFill style={{ backgroundColor: '#020202', overflow: 'hidden' }}>
       <AudioEngine category={sceneData?.theme || "default"} />
       
+      {/* STRICT PIXEL STYLES TO KILL RESIZEOBSERVER LOOPS */}
       <ThreeCanvas 
         width={3840} 
         height={2160} 
-        camera={{ position: [0, 0, 20], fov: 50 }} 
-        style={{ position: 'absolute', top: 0, left: 0 }}
+        style={{ 
+          width: '3840px', 
+          height: '2160px', 
+          position: 'absolute', 
+          top: 0, 
+          left: 0,
+          pointerEvents: 'none'
+        }}
+        camera={{ position: [0, 0, 20], fov: 50 }}
         gl={{ preserveDrawingBuffer: true, antialias: false, alpha: false }}
       >
          <MasterScene data={sceneData} />
       </ThreeCanvas>
 
       <AbsoluteFill style={{
+        position: 'absolute',
+        width: '3840px',
+        height: '2160px',
         justifyContent: 'center',
         alignItems: 'center',
         color: 'white',
@@ -31,8 +42,7 @@ export const SceneRouter = ({ sceneData }: any) => {
         textShadow: `0 0 60px ${glowColor}, 0 0 20px ${glowColor}`,
         textAlign: 'center',
         textTransform: 'uppercase',
-        zIndex: 10,
-        padding: '200px'
+        zIndex: 10
       }}>
         {title}
       </AbsoluteFill>
